cbbfacabd38a8220ac8acf4cbbf88c0e90efc799
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2023-11-13 19:29:03 +0600

    Adds a readme.md file

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Celsius to Fahrenheit Converter Tool
+
+This tool converts celsius to fahrenheit online.
+
+This is how I created a function to convert the celsius to fahrenheit:
+
+```js
+function celsiusToFahrenheit(celsius) {
+  let fahrenheit
+  fahrenheit = (celsius * 9/5) + 32
+  return fahrenheit
+}
+```
--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-// take celsius and convert to fahrenheit
+/**
+ * this function takes celsius and converts to fahrenheit
+ * @param {number} celsius 
+ * @returns {number} fahrenheit
+ */
 function celsiusToFahrenheit(celsius) {
   let fahrenheit
   fahrenheit = (celsius * 9/5) + 32
   return fahrenheit
 }
 
-// display the fahrenheit on page
+/**
+ * displays the fahrenheit on page
+ * @param {number} fahrenheit 
+ * @returns {void}
+ */
 function displayFahrenheit(fahrenheit) {
   const farSpan = document.querySelector("#far")
   farSpan.textContent = fahrenheit
 }
 
+/**
+ * utility function to handle form submit
+ * @param {Event} event 
+ * @returns {void}
+ */
 function handleFormSubmit(event) {
   event.preventDefault()
   let celsius = event.target.celsius.value 
@@ -22,6 +35,9 @@ function handleFormSubmit(event) {
 
 }
 
+/**
+ * @type {Element} form
+ */
 const form = document.querySelector("#form")
 
 form.addEventListener("submit", handleFormSubmit)
